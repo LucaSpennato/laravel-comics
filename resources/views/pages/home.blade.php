20d24a93947d016bf603a3872d2f5e7b1b006027
LucaSpennato <luca.spennato@gmail.com>
2022-09-07 19:47:03 +0200

    used forelse in home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -7,19 +7,25 @@
         <div class="hero">
             <img src="../assets/images/jumbotron.jpg"  alt="teen titans go jumbotron">
         </div>
-        
+
         <div class="container">
             <div class="row col-10 m-auto py-5 gx-3">
             
-                @foreach($comics as $comic)
-                <div class="comic my-5 col-2">
-                    <img src="{{ $comic->image }}" 
-                        alt="{{ $comic->type . ' - ' . $comic->title . '\'s poster' }}">
-                    <h6 class="mt-2">
-                        {{ $comic->title }}
-                    </h6>
-                </div>
-                @endforeach
+                @forelse($comics as $comic)
+                    <div class="comic my-5 col-2">
+                        <img src="{{ $comic->image }}" 
+                            alt="{{ $comic->type . ' - ' . $comic->title . '\'s poster' }}">
+                        <h6 class="mt-2">
+                            {{ $comic->title }}
+                        </h6>
+                    </div>
+                @empty
+
+                    <h3 class="col-12 text-center text-danger">
+                        Nessun comic disponibile
+                    </h3>
+
+                @endforelse
 
             </div>
 
