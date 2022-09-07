97cc7944cc9bd9b4146ba33173d7391a34c951a0
LucaSpennato <luca.spennato@gmail.com>
2022-09-07 17:58:24 +0200

    main scss, hero, button add

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,14 +4,29 @@
 
 @section('main-content')
     <main>
-        @foreach($comics as $comic)
-        <div class="comic my-5 col-2">
-            <img src="{{ $comic->image }}" 
-                alt="{{ $comic->type . ' - ' . $comic->title . '\'s poster' }}">
-            <h6 class="mt-2">
-                {{ $comic->title }}
-            </h6>
+        <div class="hero">
+            <img src="../assets/images/jumbotron.jpg"  alt="teen titans go jumbotron">
         </div>
-        @endforeach
+        
+        <div class="container">
+            <div class="row col-10 m-auto py-5 gx-3">
+            
+                @foreach($comics as $comic)
+                <div class="comic my-5 col-2">
+                    <img src="{{ $comic->image }}" 
+                        alt="{{ $comic->type . ' - ' . $comic->title . '\'s poster' }}">
+                    <h6 class="mt-2">
+                        {{ $comic->title }}
+                    </h6>
+                </div>
+                @endforeach
+
+            </div>
+
+        <div class="text-center pb-5">
+            <button href="#" class="btn px-5 btn-primary rounded-0">Load More</button>
+        </div>
+
+    </div>
     </main>
 @endsection
