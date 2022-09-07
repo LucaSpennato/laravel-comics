b626c0c1f805b28edc5d04d06b70823363d7ce78
LucaSpennato <luca.spennato@gmail.com>
2022-09-07 17:01:03 +0200

    header add, scss add

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,27 @@
+<header>
+    <div class="container p-3">
+      <nav class="navbar navbar-expand-lg p-0">
+        <div class="row container-fluid d-flex">
+
+          <div class="col-6 col-lg-1">
+            <a href="#"><img src="../assets/img/dc-logo.png" alt="dc-logo" /></a>
+          </div>
+
+          <ul class="col-11 d-none d-lg-block text-end">
+            <li>
+                ciao
+            </li>
+            <!-- <li v-for="(link, index) in DcComicsNavLinks" :key="index">
+              <a class="py-lg-5 px-lg-2 mx-lg-1"
+                :href="link.url"
+                :class="{ active_el: link.isActive === true }"
+                @click.prevent="navActiveOnClick(index)"
+                >{{ link.text }}</a
+              >
+            </li> -->
+          </ul>
+
+        </div>
+      </nav>
+    </div>
+  </header>
